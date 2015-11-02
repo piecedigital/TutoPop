bcbb7e71405e92c45c4a2736a75dccac5984e57b
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-11-02 01:21:51 -0500

    altered tutopop to support multiple methods. added a new method and new options

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ ___
 
 ##What is it?
 
-TutoPop is a jQuery plugin for creating simple popup tutorials showing your users the interface of your website or web app!
+TutoPop is a jQuery-based tool for creating simple popup tutorials showing your users the interface of your website or web app!
 
 ___
 
@@ -18,10 +18,35 @@ ___
 
 It's simple! All you have to do is pass in a few key points of data voila! Instant tutorials!
 
-The method takes up to 5
+The method takes up to 5 arguments
 
 ``` js
-$.tutoPop(array[, array, int, callback, callback]);
+tutoPop(array[, array, int, callback, callback]);
+```
+
+Then there are (currently) 2 initiation type methods to call on tutoPop
+
+``` js
+// timeline 
+// calling the "timeline" method will initiate tutoPop for procedural operations
+tutoPop(...).timeline([options])
+// oneTime
+// calling the "oneTIme" method will initiate tutoPop to display all tutorial popups at once.
+tutoPop(...).oneTime([options])
+```
+
+The "oneTime()" initiation method accepts an object with the following options
+``` js
+// This is an object of our initiation method options
+// highlight: indicates whether or not to include a highlight on the page
+// color: a CSS color value (hex code or RGB), the color of the area surrounding the highlighted area
+// opacity: the opacity of the highlight box (color opacity is effected)
+
+{
+  highlight: boolean,
+  opacity: number,
+  color: String
+}
 ```
 
 Here how it works:
@@ -30,17 +55,28 @@ First, you'll want to create an array of all of your tutorial prompts
 
 ``` js
 // This is an array of our tutorial prompts.
-// It's an array of objects that take 3 parameters:
-// x: this is the x coordinate for the tutorial dialog box
-// y: this is the y coordinate for the tutorial dialog box
+// It's an array of objects that take 9 parameters:
+
+// dialogX: this is the x coordinate for the tutorial dialog box
+// dialogY: this is the y coordinate for the tutorial dialog box
+// highlightX: this is the x coordinate for the tutorial highlight box
+// highlightY: this is the y coordinate for the tutorial highlight box
+// highlightRadius: this is the border radius for the highlight area
+// highlightWidth: this is the width of the optional highlight area
+// highlightHeight: this is the height of the optional highlight area
 // msg: this is the actual text for the tutorial dialog box
 // side: this is the side that arrow will appear: top(default), right, bottom, left
 
 var tutorials = [
   {
-    x: int,
-    y: int,
-    msg: String
+    dialogX: number,
+    dialogY: number,
+    highlightX: number,
+    highlightY: number,
+    highlightRadius: number,
+    highlightWidth: number,
+    highlightHeight: number,
+    msg: String,
     side: String
   },
   ...
@@ -50,7 +86,7 @@ var tutorials = [
 Next, we call the tutoPop method to initiate the tutorial popups
 
 ``` js
-$.tutoPop(tutorials);
+tutoPop(tutorials).[initiationTypeMethod]([options]);
 ```
 
 And that's it! Seriously, that's all that's required.
@@ -61,7 +97,7 @@ ___
 
 
 ``` js
-$.tutoPop(array[, array, int, callback, callback]);
+tutoPop(array[, array, number, callback, callback]);
 ```
 
 ###Color: array
@@ -73,7 +109,7 @@ The second argument accepts an array. Passing in any hex code(s) (or CSS rgb(a) 
 ```
 Pass an empty string or null to ignore that index of the array.
 
-###Index: int
+###Index: number
 
 The third argument accepts an integer. This indicates the place in the tutorials array in which to begin the tutorial.
 
@@ -91,13 +127,14 @@ Below you'll find an example of a callback function using this feature
 // This is our callback for when the user clicks the next button of the current tutorial dialog box (optional)
 
 // "arr" is the original array that gets passed back to this function
+// **NOTE** if you use the "oneTime" initiation method "arr" with actually be the one tutorial
 // "ind" is the current index that gets passed back to this function
 // "nextFunc/closeFunc" are the original callbacks that get passed back to this function
 
 var nextCB = function(arr, ind, nextFunc, closeFunc) {
   if(ind >= arr.length-1) {
     if(confirm("Would you like to rerun the tutorial?")) {
-      $.tutoPop(arr, null, null, nextFunc, closeFunc);
+      tutoPop(arr, null, null, nextFunc, closeFunc).timeline();
     }
   }
 };
@@ -108,7 +145,7 @@ var nextCB = function(arr, ind, nextFunc, closeFunc) {
 
 var closeCB = function(arr, ind, nextFunc, closeFunc) {
   if(confirm("Would you like to rerun the tutorial?")) {
-    $.tutoPop(arr, null, null, nextFunc, closeFunc);
+    tutoPop(arr, null, null, nextFunc, closeFunc).timeline();
   }
 };
 ```
--- popup-tutorial.js
+++ popup-tutorial.js
@@ -1,80 +1,216 @@
-$.tutoPop = function(tutArr, color, index, nextCB, closeCB) {
-	bgColor = "#9be0ff !important",
-	textColor = "#017ab1 !important";
-	
-	if(color && typeof color === "object") {
-		bgColor = (color[0]) ? color[0] + " !important" : bgColor,
-		textColor = (color[1]) ? color[1] + " !important" : textColor;
-	}
-	
-	tutArr = tutArr || [],
-	index = index || 0,
-	nextCB = nextCB || null,
-	closeCB = closeCB || null;
-
-	$("head").append( $("<style>").attr("id","popup-styles").text(".col-2 {width: 50%;display: inline-block;}.left-justify {text-align: left;}.right-justify {text-align: right;}.pointer {cursor: pointer;}.tut-box {position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%) scale(0.9);min-width: 15em;max-width: 15em;padding: 1em;border-radius: 1em;background-color: "+bgColor+";color: "+textColor+";box-shadow: 0 0 .4em 0 black;opacity: 0;}.tut-box.full {transition: .5s all;opacity: 1;transform: translate(-50%, -50%) scale(1);}.tut-box > div {width: 100%;height: 100%;position: relative;}.tut-box > div p {margin: 0;}.tut-box > div .separator {background-color: #004B6E;height: 1px;margin: .5em 0;opacity: 0.5;}.tut-box > div .arrow {overflow: hidden;position: absolute;width: 3.5em;height: 1.75em;}.tut-box > div .arrow.top {top: -2.75em;left: 50%;transform: translate(-50%, 0) rotateZ(0deg);}.tut-box > div .arrow.right {top: 50%;left: 100%;transform: translate(1%, -50%) rotateZ(90deg);}.tut-box > div .arrow.bottom {top: 100%;left: 50%;transform: translate(-50%, 55%) rotateZ(180deg);}.tut-box > div .arrow.left {top: 50%;left: 0%;transform: translate(-101%, -50%) rotateZ(-90deg);}.tut-box > div .arrow > div {transform: rotateZ(45deg);display: block;margin: .75em auto 0;background-color: "+bgColor+";width: 2em;height: 2em;box-shadow: 0 0 .4em 0 black;}") );
+tutoPop = function(tutArr, color, index, nextCB, closeCB) {
 	// This variable is the structure for our tutorial dialog box
 	var tutBox = function(text, x, y, side) {
 	  x = x + "px" || "50%";
 	  y = y + "px" || "50%";
 	  side = side || "top";
-	  var box = $("<div>").addClass("tut-box").css({
-	    top: y,
-	    left: x
+
+	  return $("<div>").addClass("tut-box").css({
+	  	"position": "fixed",
+	    "top": y,
+	    "left": x,
+	  	"transform": "translate(-50%, -50%) scale(0.9)",
+	  	"min-width": 15 * 16 + "px",
+	  	"max-width": 15 + 16 + "px",
+	  	"padding": "16px",
+	  	"border-radius": "16px",
+	  	"background-color": bgColor,
+	  	"color": textColor,
+	  	"box-shadow": "0 0 " + .4 * 16 + "px 0 black",
+	  	"opacity": "0"
 	  }).html(
 	    $("<div>").append(
-	      $("<div>").addClass("arrow " + side).html( $("<div>") ),
+	      $("<div>").addClass("arrow " + side).css({
+	      	"overflow": "hidden",
+	      	"position": "absolute",
+	      	"width": 3.5 * 16 + "px",
+	      	"height": 1.75 * 16 + "px",
+	      	"top": (side === "top") ? -1.75 * 16 + "px" : (side === "right") ? "50%" : (side === "bottom") ? "100%" : "50%",
+	      	"left": (side === "top") ? "50%" : (side === "right") ? "100%" : (side === "bottom") ? "50%" : "0%",
+	      	"transform": (side === "top") ? "translate(-50%, 0) rotateZ(0deg)" : (side === "right") ? "translate(-25%, -50%) rotateZ(90deg)" : (side === "bottom") ? "translate(-50%, 1%) rotateZ(180deg)" : "translate(-75%, -50%) rotateZ(-90deg)"
+	      }).html( $("<div>").css({
+	      	"transform": "rotateZ(45deg)",
+	      	"display": "block",
+	      	"margin": .75 * 16 + "px auto 0",
+	      	"background-color": bgColor,
+	      	"width": "32px",
+	      	"height": "32px",
+	      	"box-shadow": "0 0 " + .4 * 16 + "px 0 black"
+	      }) ),
 	      $("<p>").html(text),
-	      $("<div>").addClass("separator"),
-	      $("<div>").addClass("quit col-2 left-justify").html(
-	        $("<span>").addClass("pointer").html("X")
+	      $("<div>").css({
+	      	"background-color": "#004B6E",
+	      	"height": "1px",
+	      	"margin": .5 * 16 + "px 0",
+	      	"opacity": "0.5"
+	      }),
+	      $("<div>").css({
+	      	"width": "50%",
+	      	"display": "inline-block",
+	      	"text-align": "left"
+	      }).html(
+	        $("<span>").addClass("quit").css({
+	        	"cursor": "pointer"
+	        }).html("X")
 	      ),
-	      $("<div>").addClass("next col-2 right-justify").html(
-	        $("<span>").addClass("pointer").html("Next")
+	      $("<div>").css({
+	      	"width": "50%",
+	      	"display": "inline-block",
+	      	"text-align": "right"
+	      }).html(
+	        $("<span>").addClass("next").css({
+	        	"cursor": "pointer"
+	        }).html("Next")
 	      )
 	    )
 	  );
-	  
-	  return box;
 	};
 
-	// This is the function that brings together all of the components created above
-	// It initiates the tutorial dialog box.
-	// Here we pass in the array, index (optional), callback for next (optional), and callback for close (optional)
-	var tutRun = function(arr, ind, nextFunc, closeFunc) {
-	  ind = ind || 0;
-	  var thisTutBox = new tutBox(arr[ind].msg, arr[ind].x + 16, arr[ind].y + 16, arr[ind].side);
+	return {
+		timeline: function(options) {
+			options = options || {};
+			bgColor = "#9be0ff",
+			textColor = "#017ab1";
+			
+			if(color && typeof color === "object") {
+				bgColor = (color[0]) ? color[0] : bgColor,
+				textColor = (color[1]) ? color[1] : textColor;
+			}
+			
+			tutArr = tutArr || [],
+			index = index || 0,
+			nextCB = nextCB || null,
+			closeCB = closeCB || null;
 
-	  $("body").append($(thisTutBox));
-	  setTimeout(function() {
-	    $(thisTutBox).addClass("full");
-	  }, 100);
+			// box for highlighting
+			var tutHighlight = function(x, y, width, height, radius) {
+			  x = x + "px" || "50%";
+			  y = y + "px" || "50%";
+			  width = ((width + 32) || "100") + "px";
+			  height = ((height + 32) || "100") + "px";
+			  radius = (radius || "16") + "px";
 
-	  if( Math.ceil($(thisTutBox).offset().left + $(thisTutBox).width() + 16) >= $(document).width() ) {
-	    $(thisTutBox).css("left", ($(document).width() - ($(thisTutBox).width() / 2)) - 16);
-	  }
+			  opacity = options.opacity || ".5";
+			  color = options.color || "black";
 
-	  if( Math.floor($(thisTutBox).offset().left) <= 0 ) {
-	    $(thisTutBox).css("left", (0 + $(thisTutBox).width() / 2) + 16);
-	  }
-	  
-	  if( Math.ceil($(thisTutBox).offset().top + $(thisTutBox).height() + 16) >= $(document).height() ) {
-	    $(thisTutBox).offset().css("top", $(document).height() - $(thisTutBox).height());
-	  }
-	  
-	  if( Math.floor($(thisTutBox).offset().top) <= 0 ) {
-	    $(thisTutBox).offset().css("top", 0 + 16);
-	  }
-	  
-	  $(thisTutBox).find(".next").on("click", function() {
-	    $(thisTutBox).remove();
-	    if(nextFunc && typeof nextFunc === "function") { nextFunc(arr, ind, nextFunc, closeFunc); };
-	    if(ind < arr.length-1) { tutRun(arr, ind+1, nextFunc, closeFunc); nextFunc = null; };
-	  });
-	  $(thisTutBox).find(".quit").on("click", function() {
-	    if(closeFunc && typeof closeFunc === "function") { closeFunc(arr, ind, nextFunc, closeFunc); nextFunc = null; };
-	    $(thisTutBox).remove();
-	  });
+			  return $("<div>").addClass("tuto-highlight").css({
+			    "position": "fixed",
+			    "top": y,
+			    "left": x,
+			    "width": width,
+			    "height": height,
+			  	"transform": "translate(-50%, -50%)",
+			  	"box-shadow": "0 0 0 1920px " + color + ", inset 0 0 16px 10px " + color,
+          "border-radius": radius,
+          "opacity": opacity
+			  });
+			};
+
+			// This is the function that brings together all of the components created above
+			// It initiates the tutorial dialog box.
+			// Here we pass in the array, index (optional), callback for next (optional), and callback for close (optional)
+			var tutRun = function(arr, ind, nextFunc, closeFunc) {
+			  ind = ind || 0;
+			  var thisHLBox = (options.highlight) ? new tutHighlight(arr[ind].highlightX + 16, arr[ind].highlightY + 16, arr[ind].highlightWidth, arr[ind].highlightHeight, arr[ind].highlightRadius) : "";
+			  var thisTutBox = new tutBox(arr[ind].msg, arr[ind].dialogX + 16, arr[ind].dialogY + 16, arr[ind].side);
+
+			  $("body").append($(thisHLBox), $(thisTutBox));
+			  setTimeout(function() {
+			    $(thisTutBox).css({
+			    	"transition": ".5s all",
+			    	"opacity": "1",
+			    	"transform": "translate(-50%, -50%) scale(1)"
+			    });
+			  }, 100);
+
+			  if( Math.ceil($(thisTutBox).offset().left + $(thisTutBox).width() + 16) >= $(document).width() ) {
+			    $(thisTutBox).css("left", ($(document).width() - ($(thisTutBox).width() / 2)) - 16);
+			  }
+
+			  if( Math.floor($(thisTutBox).offset().left) <= 0 ) {
+			    $(thisTutBox).css("left", (0 + $(thisTutBox).width() / 2) + 16);
+			  }
+			  
+			  if( Math.ceil($(thisTutBox).offset().top + $(thisTutBox).height() + 16) >= $(document).height() ) {
+			    $(thisTutBox).offset().css("top", $(document).height() - $(thisTutBox).height());
+			  }
+			  
+			  if( Math.floor($(thisTutBox).offset().top) <= 0 ) {
+			    $(thisTutBox).offset().css("top", 0 + 16);
+			  }
+			  
+			  $(thisTutBox).find(".next").on("click", function() {
+			  	$(thisHLBox).remove();
+			  	thisHLBox = null;
+			    $(thisTutBox).remove();
+			    thisTutBox = null;
+			    if(nextFunc && typeof nextFunc === "function") { nextFunc(arr, ind, nextFunc, closeFunc); };
+			    if(ind < arr.length-1) { tutRun(arr, ind+1, nextFunc, closeFunc); nextFunc = null; };
+			  });
+			  $(thisTutBox).find(".quit").on("click", function() {
+			    if(closeFunc && typeof closeFunc === "function") { closeFunc(arr, ind, nextFunc, closeFunc); nextFunc = null; };
+			    $(thisHLBox).remove();
+			    thisHLBox = null;
+			    $(thisTutBox).remove();
+			    thisTutBox = null;
+			  });
+			}
+			tutRun(tutArr, index, nextCB, closeCB);
+		},
+		oneTime: function(options) {
+			bgColor = "#9be0ff",
+			textColor = "#017ab1";
+			
+			if(color && typeof color === "object") {
+				bgColor = (color[0]) ? color[0] : bgColor,
+				textColor = (color[1]) ? color[1] : textColor;
+			}
+			
+			tutArr = tutArr || [],
+			index = index || 0,
+			nextCB = nextCB || null,
+			closeCB = closeCB || null;
+
+			// This is the function that brings together all of the components created above
+			// It initiates the tutorial dialog box.
+			// Here we pass in the array, index (optional), callback for next (optional), and callback for close (optional)
+			var tutRun = function(tut, ind, nextFunc, closeFunc) {
+			  ind = ind || 0;
+			  var thisTutBox = new tutBox(tut.msg, tut.x + 16, tut.y + 16, tut.side);
+
+			  $("body").append($(thisTutBox));
+			  setTimeout(function() {
+			    $(thisTutBox).addClass("full");
+			  }, 100);
+
+			  if( Math.ceil($(thisTutBox).offset().left + $(thisTutBox).width() + 16) >= $(document).width() ) {
+			    $(thisTutBox).css("left", ($(document).width() - ($(thisTutBox).width() / 2)) - 16);
+			  }
+
+			  if( Math.floor($(thisTutBox).offset().left) <= 0 ) {
+			    $(thisTutBox).css("left", (0 + $(thisTutBox).width() / 2) + 16);
+			  }
+			  
+			  if( Math.ceil($(thisTutBox).offset().top + $(thisTutBox).height() + 16) >= $(document).height() ) {
+			    $(thisTutBox).offset().css("top", $(document).height() - $(thisTutBox).height());
+			  }
+			  
+			  if( Math.floor($(thisTutBox).offset().top) <= 0 ) {
+			    $(thisTutBox).offset().css("top", 0 + 16);
+			  }
+			  
+			  $(thisTutBox).find(".next").on("click", function() {
+			    $(thisTutBox).remove();
+			    if(nextFunc && typeof nextFunc === "function") { nextFunc(tut, ind, nextFunc, closeFunc); };
+			  });
+			  $(thisTutBox).find(".quit").on("click", function() {
+			    if(closeFunc && typeof closeFunc === "function") { closeFunc(tut, ind, nextFunc, closeFunc); nextFunc = null; };
+			    $(thisTutBox).remove();
+			  });
+			}
+			for(var tut in tutArr) {
+				tutRun(tutArr[tut], index, nextCB, closeCB);
+			}
+		}
 	}
-	tutRun(tutArr, index, nextCB, closeCB);
 };
